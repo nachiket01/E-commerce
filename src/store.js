ced3a4f20326d23aa793568693cde9a0231e69f7
nachiket01 <nachikettekade01@gmail.com>
2022-12-16 20:24:14 +0530

    working not

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./feature/cart-slice";
-import cartReducer from "./feature/cart-slice"
+import cartReducer from "./feature/cart-slice";
 
 export const store = configureStore({
-    reducer: cartReducer
-    
-})
+  reducer: {
+    cart: cartReducer,
+  },
+});
